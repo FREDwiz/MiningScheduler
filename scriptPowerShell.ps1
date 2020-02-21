dd761d723ba4db2c0881a67cffa07a38d6ee3ba2
FREDwiz <fumbrella@gmail.com>
2020-02-21 14:39:13 +0100

    Update scriptPowerShell.ps1

--- scriptPowerShell.ps1
+++ scriptPowerShell.ps1
@@ -1,14 +1,14 @@
 $Echo
 
-cd "C:\Users\federico.umbrella\Desktop\xmr-stak-win64"
+cd "<path-to-your-mining-file>"
  
 while($true)
 {
 	Echo "Starting new mining session... "
-	START "C:\Users\federico.umbrella\Desktop\xmr-stak-win64\xmr-stak.exe"
+	START "<your-mining-file>"
 	Echo "Running for 8 hours"
 	TIMEOUT /T 28800
-	stop-process -Name "xmr-stak"
+	stop-process -Name "<process-name>"
 	Echo "Resting for 2 hours... "
 	TIMEOUT /T 7200
 	Echo "Here we go again!"
